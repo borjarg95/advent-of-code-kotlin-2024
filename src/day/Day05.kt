ab6a2b32ea12769f1c5604b98314fa21e6066116
borjarg95 <borja-r-g@hotmail.com>
2024-12-06 01:03:08 +0100

    Day 5 part 1, not time to part2

--- src/day/Day05.kt
+++ src/day/Day05.kt
@@ -1,33 +1,45 @@
 package day
 
-typealias Updates = List<List<Int>>
+
 typealias Notes = Map<Int, MutableSet<Int>>
 
 data object Day05 : Day {
-    private fun List<String>.parseInput(): Pair<Notes, Updates> {
-        val notes  = mutableMapOf<Int, MutableSet<Int>>()
+    private fun List<String>.parseInput(): Pair<Notes, List<List<Int>>> {
+        val notes = mutableMapOf<Int, MutableSet<Int>>()
         val updates = mutableListOf(mutableListOf<Int>())
         forEach { line ->
             when {
                 line.contains('|') ->
                     line.split('|')
-                        .let { (x, y) -> notes[x.toInt()]?.add(y.toInt()) ?: mutableSetOf(y.toInt()) }
+                        .let { (x, y) ->
+                            notes.compute(x.toInt()) { _, values ->
+                                values?.add(y.toInt())?.let { values } ?: mutableSetOf(y.toInt())
+                            }
+                        }
                 line.contains(',') ->
                     line.split(',')
                         .mapTo(mutableListOf(), String::toInt)
                         .let(updates::add)
             }
-
         }
         return notes to updates
     }
 
+    private fun List<Int>.middlePageNumber(): Int = this[this.size / 2]
+
+    private fun List<Int>.isOrdered(notes: Notes): Boolean {
+        val checked = mutableSetOf<Int>()
+        return takeIf(List<Int>::isNotEmpty)?.all { value ->
+            (notes.getOrDefault(value, emptySet()).none(checked::contains)
+                && checked.all { midValue -> notes[midValue]?.contains(value) == true }
+            ).also { checked.add(value) }
+        } ?: false
+    }
 
     override fun part1(input: List<String>): Int {
-        TODO("Not yet implemented")
+        val (notes, updates) = input.parseInput()
+        return updates.filter { it.isOrdered(notes) }.sumOf { it.middlePageNumber() }
     }
 
-    override fun part2(input: List<String>): Int {
-        TODO("Not yet implemented")
-    }
+    override fun part2(input: List<String>): Int = 0
 }
