39eab98213b95a6ff59b6c3f3ee00289a14a20de
borja_rg <borja-r-g@hotmail.com>
2024-12-08 16:01:01 +0100

    Day07 part2 with operations refactor

--- src/day/Day07.kt
+++ src/day/Day07.kt
@@ -1,6 +1,5 @@
 package day
 
-import java.awt.SecondaryLoop
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.asFlow
@@ -8,7 +7,48 @@ import kotlinx.coroutines.flow.flatMapMerge
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.toList
 
+typealias Operation = (Long, Long) -> Long
+
 data object Day07: Day {
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    override suspend fun part1(input: List<String>): Long {
+        return input.parseInput().flatMapMerge { (result, values) ->
+            flow {
+                val value = if (result.isCorrect(values, listOf(sum, mul))) result else 0
+                emit(value)
+            }
+        }.toList()
+        .sum()
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    override suspend fun part2(input: List<String>): Long {
+        return input.parseInput()
+            .flatMapMerge { (result, values) ->
+                flow {
+                    val value = if (result.isCorrect(values, listOf(sum, mul, concat))) result else 0
+                    emit(value)
+                }
+            }.toList()
+            .sum()
+    }
+
+    private fun Long.isCorrect(
+        values: List<Long>,
+        operations: List<Operation>,
+        index: Int = 0,
+        acc: Long = 0,
+    ): Boolean {
+        return when {
+            acc > this -> false
+            index == values.size -> acc == this
+            else -> operations.any {
+                isCorrect(values, operations = operations, index = index + 1, acc = it(acc, values[index]))
+            }
+        }
+    }
+
     @OptIn(ExperimentalCoroutinesApi::class)
     private fun List<String>.parseInput(): Flow<Pair<Long, List<Long>>> = this.asFlow()
         .flatMapMerge { line ->
@@ -17,30 +57,8 @@ data object Day07: Day {
                 emit(result.toLong() to numbers.trim().split(' ').map(String::toLong))
             }
         }
-    @OptIn(ExperimentalCoroutinesApi::class)
-    override suspend fun part1(input: List<String>): Long {
-        return input.parseInput().flatMapMerge { (result, values) ->
-            flow {
-                val value = if (isCorrect(values, 0, expected = result)) result else 0
-                emit(value)
-            }
-        }.toList()
-        .sum()
-    }
 
-    override suspend fun part2(input: List<String>): Long {
-        TODO("Not yet implemented")
-    }
-
-    private fun isCorrect(
-        values: List<Long>,
-        index: Int,
-        acc: Long = 0,
-        expected: Long
-    ): Boolean {
-        if (index == values.size) return acc == expected
-
-        return isCorrect(values, index+1, acc + values[index], expected)
-            || isCorrect(values, index+1, acc * values[index], expected)
-    }
+    private val sum: Operation = { a, b -> a + b}
+    private val mul: Operation = { a, b -> a * b}
+    private val concat: (Long, Long) -> Long = { a, b -> "$a$b".toLong() }
 }
